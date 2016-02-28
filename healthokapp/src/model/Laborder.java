e27671730a049315294b8eac16f0f572ec9c345f
Vikash Kumar <vikashgupta940@gmail.com>
2016-02-28 15:13:11 +0530

    commit

---
+++ healthokapp/src/model/Laborder.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Laborder {
+	private int labresultimageId;
+	private int prescriptionimageId;
+	private String disription;
+	private int orderId;
+	private int laborderId;
+	public int getLabresultimageId() {
+		return labresultimageId;
+	}
+	public void setLabresultimageId(int labresultimageId) {
+		this.labresultimageId = labresultimageId;
+	}
+	public int getPrescriptionimageId() {
+		return prescriptionimageId;
+	}
+	public void setPrescriptionimageId(int prescriptionimageId) {
+		this.prescriptionimageId = prescriptionimageId;
+	}
+	public String getDisription() {
+		return disription;
+	}
+	public void setDisription(String disription) {
+		this.disription = disription;
+	}
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getLaborderId() {
+		return laborderId;
+	}
+	public void setLaborderId(int laborderId) {
+		this.laborderId = laborderId;
+	}
+	
+
+}
