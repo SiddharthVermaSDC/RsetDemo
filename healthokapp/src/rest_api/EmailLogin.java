b29271a29bd1e0f2ee63aa3ad205d771915cce9a
shailendrasv <shailuverma1111995@gmail.com>
2016-02-29 19:55:45 +0530

    err

--- healthokapp/src/rest_api/EmailLogin.java
+++ healthokapp/src/rest_api/EmailLogin.java
@@ -21,16 +21,16 @@ public class EmailLogin
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public int loginCheck(GetSetLogin gs)
+	public Message loginCheck(GetSetLogin gs)
 	{
 		  Connection connection = null;
 		  PreparedStatement preparedStatement=null;
 		  ResultSet resultSet=null;
-		  int status;
+		  Message mg=new Message();
 		  try {
 			connection = DatabaseConnectivity.getInstance().getConnection();
-		    String query1="select UserId from User where EmailId=\""+gs.getEmail()+"\" and password=\""+gs.getPassword()+"\"";
-		    String query2="select UserId from User where Mobile=\""+gs.getPhone()+"\" and password=\""+gs.getPassword()+"\"";
+		    String query1="select * from User where EmailId=\""+gs.getEmail()+"\" and password=\""+gs.getPassword()+"\"";
+		    String query2="select * from User where Mobile=\""+gs.getPhone()+"\" and password=\""+gs.getPassword()+"\"";
 		   if(gs.getEmail()==null)
 		   {
 			   preparedStatement = (PreparedStatement)connection.prepareStatement(query2);	
@@ -40,18 +40,30 @@ public class EmailLogin
 			   preparedStatement =(PreparedStatement)connection.prepareStatement(query1);
 		   }
 			 resultSet=preparedStatement.executeQuery();
+<<<<<<< HEAD
 			 
 			 if(resultSet.next())
 				  status=resultSet.getInt("UserId");
+=======
+			 if(resultSet.next()==true)
+				 mg.setMessage("Logged in successfully!");
+>>>>>>> parent of af8d69e... new
 			 else
-				status=-1;
+				 mg.setMessage("New User. Please register first.");
 	     }
 	     catch(Exception e)
 	     {
-	    	status=500;
+	    	 mg.setMessage(e.getMessage());
 	     }
-		return status;
+		return mg;
 	}
 	
-	
+	@Path("/msg")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMethod()
+	{
+	    String s="bbb";
+		return s;
+}
 }
