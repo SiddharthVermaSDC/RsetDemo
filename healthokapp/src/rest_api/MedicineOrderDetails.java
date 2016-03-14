9a004af3dfecc73bc8a4f8e1149b5d30378a7746
Vikash Kumar <vikashgupta940@gmail.com>
2016-03-14 17:27:56 +0530

    commit

--- healthokapp/src/rest_api/MedicineOrderDetails.java
+++ healthokapp/src/rest_api/MedicineOrderDetails.java
@@ -1,35 +1,30 @@
 package rest_api;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.ws.rs.DELETE;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import model.Result;
 
 @Path("/medicineorderdetails")
 public class MedicineOrderDetails {
 	
-	@Path("/{MedicineOrderDetailsId}/{MedicineOrderId}/{MedicineName}/{Dosage}/{Quantity}/{Price}")
-	@GET
+	@Path("/placeorder")
+	@POST
 	@Produces (MediaType.APPLICATION_JSON)
-	public static model.Result medicineorderdetails(@PathParam("MedicineOrderDetailsId") int medicineorderdetailsid,@PathParam("MedicineOrderId") int medicineorderid,@PathParam("MedicineName") String medicinename,@PathParam("Dosage") String dosage,@PathParam("Quantity") int quantity,@PathParam("Price") int price){
-	model.MedicineOrderDetails medicineorderdetails=new model.MedicineOrderDetails();
+	@Consumes (MediaType.APPLICATION_JSON)
+	public static model.Result medicineorderdetails(model.MedicineOrderDetails morder){
+	//model.MedicineOrderDetails medicineorderdetails=new model.MedicineOrderDetails();
 	model.Result res=new model.Result();
 	
-	medicineorderdetails.setMedicineOrderDetailsId(medicineorderid);
-	medicineorderdetails.setMedicineOrderId(medicineorderid);
-    medicineorderdetails.setMedicineName(medicinename);
-	medicineorderdetails.setDosage(dosage);
-	medicineorderdetails.setQuantity(quantity);
-	medicineorderdetails.setPrice(price);
-	 res.setStatus(dal.MedicineOrderDetails.insertMedicineOrderDetails(medicineorderdetails));
+	
+	 res.setStatus(dal.MedicineOrderDetails.insertMedicineOrderDetails(morder));
 	 return res;
 	}	
 	 @Path("/{medicineorderid}")
