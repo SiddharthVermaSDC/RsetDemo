c80e73ab511351ce70836757f769c9cc122bdd8f
AbhayHealthOK <abhay-jaiswal@health-ok.in>
2016-04-02 15:40:53 +0530

    code cleanup

--- healthokapp/src/biz/DoctorRetrival.java
+++ healthokapp/src/biz/DoctorRetrival.java
@@ -13,45 +13,6 @@ public class DoctorRetrival {
 		return new dal.DoctorRetrival().searchDoctor(searchString);
 	}
 	
-	
-	public static ArrayList<model.DoctorRetrival> getDoctor(String fname, String mname, String lname, int count) {
-		ArrayList<model.DoctorRetrival> docs;
-		int rank;
-		docs = new dal.DoctorRetrival().responseDoctor(fname, mname, lname, count);
-		// int n=docs.size;
-		if (count == 3) {
-			for (model.DoctorRetrival doc : docs) {
-				rank = 0;
-				if (doc.getFirstName().contains(fname) || doc.getMiddleName().contains(fname)
-						|| doc.getLastName().contains(fname))
-					doc.setRank(++rank);
-				if (doc.getFirstName().contains(mname) || doc.getMiddleName().contains(mname)
-						|| doc.getLastName().contains(mname))
-					doc.setRank(++rank);
-				if (doc.getFirstName().contains(lname) || doc.getMiddleName().contains(lname)
-						|| doc.getLastName().contains(lname))
-					doc.setRank(++rank);
-			}
-		} else if (count == 2) {
-			for (model.DoctorRetrival doc : docs) {
-				rank = 0;
-				if (doc.getFirstName().contains(fname) || doc.getMiddleName().contains(fname)
-						|| doc.getLastName().contains(fname))
-					doc.setRank(++rank);
-				if (doc.getFirstName().contains(mname) || doc.getMiddleName().contains(mname)
-						|| doc.getLastName().contains(mname))
-					doc.setRank(++rank);
-			}
-		} else if (count == 1) {
-			for (model.DoctorRetrival doc : docs) {
-				rank = 0;
-				if (doc.getFirstName().contains(fname) || doc.getMiddleName().contains(fname)
-						|| doc.getLastName().contains(fname))
-					doc.setRank(++rank);
-			}
-		}
-		return docs;
-	}
 
 	public  ArrayList<model.Doctor> getDoctorBySpeciality(int speciality) {
 		return new dal.DoctorRetrival().searchDoctorBySpeciality(speciality);
