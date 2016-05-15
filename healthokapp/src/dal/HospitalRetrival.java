45ef3ed601aa0f8e7e25c6a527e5871e991699ec
SiddharthVermaSDC <siddharthv23@gmail.com>
2016-05-14 17:21:25 -0700

    commit
    commit

--- healthokapp/src/dal/HospitalRetrival.java
+++ healthokapp/src/dal/HospitalRetrival.java
@@ -12,81 +12,63 @@ import util.Logging;
 
 public class HospitalRetrival {
 
+	public model.Hospital responseHospital(int hospitalId) {
 
-	public  model.Hospital responseHospital(int hospitalId) {
-		
-		 Connection connection = null;
-		 PreparedStatement ps  = null;
-		 ResultSet rs  = null;
+		Connection connection = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		String sql = "Select * from hospital where HospitalId=\"" + hospitalId + "\"";
+		model.Hospital hospital = null;
 
 		try {
-				connection = Database.createConnection();
-			String sql = "Select * from hospital where HospitalId=\"" + hospitalId + "\"";
+			connection = Database.createConnection();
 			ps = connection.prepareStatement(sql);
+			ps.setInt(1, hospitalId);
 			rs = ps.executeQuery();
 			System.out.println(rs);
 			while (rs.next()) {
-				int dbHospitalId = rs.getInt("HospitalId"); // this is useless as this is same as passed in hospitalId
-				String hospitalname = rs.getString("Name");
-				int addressId = rs.getInt("AddressId");
-				boolean hasER = rs.getBoolean("HasER");
-				String facilities = rs.getString("Facilities");
-				int opdFees = rs.getInt("OPDFees");
-				int bed = rs.getInt("Beds");
-				String addressLine1 = rs.getString("AddressLine1");
-				String addressLine2 = rs.getString("AddressLine2");
-				String addressLine3 = rs.getString("AddressLine3");
-				int cityId = rs.getInt("CityId");
-				String pincode = rs.getString("PinCode");
-				Date regDate = rs.getDate("RegistrationDate");
-				String website = rs.getString("Website");
-				String phonenumber = rs.getString("Hospitalphonenumber");
-				boolean hasRadiology = rs.getBoolean("Hasradiology");
-				boolean hasDiagnistics = rs.getBoolean("Hasdiagnistics");
-				boolean hasAmbulance = rs.getBoolean("Hasambulance");
-				String addmissionProcess = rs.getString("AdmissionProcess");
-				return new model.Hospital(hospitalId, hospitalname, addressId, hasER, facilities, opdFees, bed,
-						addressLine1, addressLine2, addressLine3, cityId, pincode, regDate, website, phonenumber,
-						hasRadiology, hasDiagnistics, hasAmbulance, addmissionProcess);
+				
+				hospital=new model.Hospital(hospitalId);
+				hospital.setHospitalId(hospitalId);
+				hospital.setHospitalname(rs.getString("hospitalname"));
+				hospital.setRegDate(rs.getDate("RegDate"));
+				hospital.setWebsite(rs.getString("website"));
+				hospital.setHasRadiology(rs.getBoolean("HasRadiology"));
+				hospital.setHasDiagnistics(rs.getBoolean("Hasdiagnistics"));
 
-				// hosptl.add(new model.Hospital(cityId, hospitalname,addressId,
-				// hasER, facilities, opdFees, bed, addressLine1, addressLine2,
-				// addressLine3, cityId, pincode, regDate, website, phonenumber,
-				// hasRadiology, hasDiagnistics, hasAmbulance,
-				// addmissionProcess));
 			}
 		} catch (SQLException e) {
 			Logging.Exception("HOSPITALDAL", "SQL Error " + e.getMessage());
 
-		}
-		finally
-		{
+		} finally {
 			Database.closeConnection(connection);
 		}
 		return null;
 	}
 
-	// return all hospitals - Needs to be fixed to return arraylist of all hospitals. 
-	
-	public  ArrayList<Hospital> allHospitals() {
+	// return all hospitals - Needs to be fixed to return arraylist of all
+	// hospitals.
+
+	public ArrayList<Hospital> allHospitals() {
 		ArrayList<model.Hospital> hosptl1 = new ArrayList<model.Hospital>();
 
-		 Connection connection;
-		 PreparedStatement ps;
-		 ResultSet rs;
+		Connection connection;
+		PreparedStatement ps;
+		ResultSet rs;
 
 		try {
 			connection = Database.createConnection();
 			String sql = "Select * from hospital";
 			ps = connection.prepareStatement(sql);
 			rs = ps.executeQuery();
-			//System.out.println(rs1);
+			// System.out.println(rs1);
 			while (rs.next()) {
 				int hospitalId = rs.getInt("HospitalId");
 				hosptl1.add(new model.Hospital(hospitalId));
 				// add code to fill all details of hospital
 			}
-		} catch (SQLException  e) {
+		} catch (SQLException e) {
 			System.out.println("SQL EXCEPTION**2");
 		}
 		return hosptl1;
